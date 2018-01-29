f58174189e8d2985412a736f25f86fba4f18c2f8
Zhach Volker <zhach@zhach99.roam.corp.google.com>
2018-01-28 22:18:24 -0500

    JS Beautify

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let act = new ReLU();
-let nn = new NN([2, 2, 2], act, 1e-7);
+let act = new Sigmoid();
+let nn = new NN([2, 4, 2], act, 1e-7);
 let epochs = 10000;
 let batch_size = 1000;
 let train_size = 10000;
@@ -7,12 +7,12 @@ let test_size = 1000;
 
 // Sum is greater than 0.5
 let y1 = function(...x) {
-  return (x[0].reduce(getSum) > 0.5 ? 1 : 0);
+	return (x[0].reduce(getSum) > 0.5 ? 1 : 0);
 }
 
 // Product is less than 0.5
-let y2 = function (...x) {
-  return (x[0].reduce(getProduct) < 0.5 ? 1 : 0);
+let y2 = function(...x) {
+	return (x[0].reduce(getProduct) < 0.5 ? 1 : 0);
 }
 
 // Make train and test
@@ -21,50 +21,58 @@ let train_y = [];
 let test_X = [];
 let test_y = [];
 for (var i = 0; i < train_size; i++) {
-  let inputs = [[Math.random()],
-                [Math.random()]];
-  let answer = [[y1(inputs.reduce(flatten))],
-                [y2(inputs.reduce(flatten))]];
-  let input_mat = new Matrix();
-  input_mat.set(inputs);
-  let answer_mat = new Matrix();
-  answer_mat.set(answer);
-  train_X.push(input_mat);
-  train_y.push(answer_mat);
+	let inputs = [
+		[Math.random()],
+		[Math.random()]
+	];
+	let answer = [
+		[y1(inputs.reduce(flatten))],
+		[y2(inputs.reduce(flatten))]
+	];
+	let input_mat = new Matrix();
+	input_mat.set(inputs);
+	let answer_mat = new Matrix();
+	answer_mat.set(answer);
+	train_X.push(input_mat);
+	train_y.push(answer_mat);
 }
 for (var i = 0; i < test_size; i++) {
-  let inputs = [[Math.random()],
-                [Math.random()]];
-  let answer = [[y1(inputs.reduce(flatten))],
-                [y2(inputs.reduce(flatten))]];
-  let input_mat = new Matrix();
-  input_mat.set(inputs);
-  let answer_mat = new Matrix();
-  answer_mat.set(answer);
-  test_X.push(input_mat);
-  test_y.push(answer_mat);
+	let inputs = [
+		[Math.random()],
+		[Math.random()]
+	];
+	let answer = [
+		[y1(inputs.reduce(flatten))],
+		[y2(inputs.reduce(flatten))]
+	];
+	let input_mat = new Matrix();
+	input_mat.set(inputs);
+	let answer_mat = new Matrix();
+	answer_mat.set(answer);
+	test_X.push(input_mat);
+	test_y.push(answer_mat);
 }
 
 // train with minibatch!
 for (let i = 0; i < epochs; i++) {
-  let inputs_list = [];
-  let answer_list = [];
+	let inputs_list = [];
+	let answer_list = [];
 
-  let batch = make_batch(train_X, train_y, batch_size);
+	let batch = make_batch(train_X, train_y, batch_size);
 
-  if (i % 1000 == 0) {
-    console.log(nn.train(batch[0], batch[1]));
-  } else {
-    nn.train(batch[0], batch[1]);
-  }
+	if (i % 1000 == 0) {
+		console.log(nn.train(batch[0], batch[1]));
+	} else {
+		nn.train(batch[0], batch[1]);
+	}
 }
 
 let error_sum = 0;
 for (let i = 0; i < test_size; i++) {
-  let guess = nn.guess(test_X[i]);
-  let neg_outs = Matrix.scale(guess, -1);
-  let pre_square = Matrix.add(test_y[i], neg_outs);
-  error_sum += Matrix.map(pre_square, square).sum();
+	let guess = nn.guess(test_X[i]);
+	let neg_outs = Matrix.scale(guess, -1);
+	let pre_square = Matrix.add(test_y[i], neg_outs);
+	error_sum += Matrix.map(pre_square, square).sum();
 }
 
-console.log("Validation:", error_sum/test_size);
+console.log("Validation:", error_sum / test_size);
--- utils.js
+++ utils.js
@@ -1,31 +1,36 @@
 function isIntegerAbove0(x) {
-  return typeof x === "number" && x > 0 && Number.isInteger(x);
+	return typeof x === "number" && x > 0 && Number.isInteger(x);
 }
 
 function getSum(total, num) {
-    return total + num;
+	return total + num;
 }
 
 function getProduct(total, num) {
-    return total * num;
+	return total * num;
 }
 
 function flatten(prev, curr) {
-  return prev.concat(curr);
+	return prev.concat(curr);
 }
 
 function square(x) {
-  return x*x;
+	return x * x;
 }
 
 function make_batch(X, y, batch_size) {
-  let size_of_set = X.length;
-  let batch_X = [];
-  let batch_y = [];
-  for (var i = batch_size - 1; i >= 0; i--) {
-    let index = Math.floor(Math.random() * size_of_set);
-    batch_X.push(X.slice(index, index+1)[0])
-    batch_y.push(y.slice(index, index+1)[0])
-  }
-  return [batch_X, batch_y];
+	let size_of_set = X.length;
+	let batch_X = [];
+	let batch_y = [];
+	for (var i = batch_size - 1; i >= 0; i--) {
+		let index = Math.floor(Math.random() * size_of_set);
+		batch_X.push(X.slice(index, index + 1)[0])
+		batch_y.push(y.slice(index, index + 1)[0])
+	}
+	return [batch_X, batch_y];
+}
+
+function print(value) {
+	var precision = 14;
+	console.log(math.format(value, precision));
 }
